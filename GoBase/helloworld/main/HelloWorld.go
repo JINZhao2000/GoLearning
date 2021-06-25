86c05c90eb7fc45e8ffa257a3989dca6a08090d6
JIN Zhao <jinzhaofr@outlook.com>
2021-06-25 23:29:31 +0200

    go

---
+++ GoBase/helloworld/main/HelloWorld.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+/**
+Hello World
+ */
+func main() {
+	for i := 0; i < 10; i++ {
+		if i%2 == 0 {
+			// Hello World
+			/*
+			Hello World
+			 */
+			fmt.Println("Hello World")
+		}
+	}
+	a := 0
+	//var a = 0
+	fmt.Println(a)
+}
